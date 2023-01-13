eb995fd25ae17b543d3e2c85bf8a46600330ca0c
TamaraHunanyan <tamara.mkrtchyan@gmail.com>
2023-01-12 21:28:31 -0800

    Convefted to unittest module

--- InvertArray.py
+++ InvertArray.py
@@ -1,11 +1,7 @@
+import unittest
 
 def invertArray(arr):
     halfLength = len(arr) // 2
     for idx in range(halfLength):
         arr[idx], arr[-idx - 1] = arr[-idx - 1], arr[idx]
 
-def run():
-    arr = ['p', 'y', 't', 'h', 'o', 'n']
-    print("Before", arr)
-    invertArray(arr)
-    print("After", arr)
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-import InvertArray
+import unittest
+from InvertArray import invertArray
+class TestStringMethods(unittest.TestCase):
+    def test_invertArray(self):
+        arr = []
+        invertArray(arr)
+        self.assertEqual(arr, [])
+        arr = ['a']
+        invertArray(arr)
+        self.assertEqual(arr, ['a'])
+        arr = ['a', 'b']
+        invertArray(arr)
+        self.assertEqual(arr, ['b', 'a'])
+        arr = ['a', 'r', 'm', 'a', 'n']
+        invertArray(arr)
+        self.assertEqual(arr, ['n', 'a', 'm', 'r', 'a'])
 
 if __name__ == '__main__':
-    InvertArray.run()
+    unittest.main()
 
 
